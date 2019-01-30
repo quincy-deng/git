925660624274a1f5aaa24195255e78d7c4a0a573
dengqiuyang <395319225@qq.com>
2019-01-30 11:42:19 +0800

    add species ranking method in ptg_type file;no testing,estimate need to major change codes

--- 01.不同采样部位病原菌水平分析/08.Training/html_analysis.py
+++ 01.不同采样部位病原菌水平分析/08.Training/html_analysis.py
@@ -2,6 +2,7 @@ import os,shutil
 import xlrd
 import pandas as pd
 from collections import defaultdict,Counter
+import numpy as np
 # >> Read table <<
 table=r'D:\0125\08.resultdata\a.华山医院给出NGS结果\12.5重新整理中英文.xlsx'
 sheets=xlrd.open_workbook(table).sheet_names()
@@ -10,4 +11,26 @@ patientsID={row[0][:10]:{i.split(',')[1]:i.split(',')[0] for i in row[1].split('
 for idt,ptgs in patientsID.items():
     if idt in dataids:
         os.chdir(idt)
-        temp = [j for i in [pd.read_table(i,encoding='gbk',engine='python')['SDSMRN'].tolist() for i in os.listdir() if not i.endswith('xlsx')] for j in i]
+        fls = os.listdir()
+        for ptg,typ in ptgs.items():
+            # 病原文件类型
+            fl = [i for i in fls if not i.endswith('xlsx') if i.split('.')[1]==typ]
+            if any(fl):
+                df = pd.read_table(fl[0],engine='python',encoding='gbk')
+                # species 类型
+                spe = [i for i in df['Species'] if i==ptg]
+                genus = [i for i in df['Species'] if i.split('_')[0]==ptg.split('_')[0]]
+                if any(spe):
+                    index,x = list(df['Species']).index(spe[0]),list(df['SDSMRN'])
+                    sdsmrn = x[index]
+                    rank = np.array(x)[np.argsort(-np.array(x))].index(int(sdsmrn))+1
+                elif any(genus):
+                    x,y = list(df['Species']),list(df['SDSMRN'])
+                    index = [x.index(i) for i in genus]
+                    sdsmrn = sum([x[i] for i in index])
+                    new_list = y+[sdsmrn]
+                    rank = np.array(new_list)[np.argsort(-np.array(new_list))].index(int(sdsmrn))+1
+                else:
+                    print(ptg,idt)
+    else:
+        print('lack {} sample file!'.format(idt))
